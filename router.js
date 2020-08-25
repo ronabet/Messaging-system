832e3b380de642b75c737dcd2c09ba8f2de96b91
Ronabet <ronabet456@gmail.com>
2020-08-26 01:08:54 +0300

    add / route

--- router.js
+++ router.js
@@ -1,8 +1,10 @@
 const userRouter = require('./routers/userRouter');
 const messageRouter = require('./routers/messageRouter');
 const express = require('express');
+const { route } = require('./routers/messageRouter');
 const router = express.Router();
 
+router.use('/', (req, res) => { res.send("Welcome for messaging service server!")})
 router.use('/api/users', userRouter);
 router.use('/api/messages', messageRouter);
 
